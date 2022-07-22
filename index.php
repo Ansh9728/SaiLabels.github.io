575dc351c5a17fedd3527eb2fd7cca135f5c70ee
Anshu Kumar Singh <69234255+Ansh9728@users.noreply.github.com>
2022-07-22 16:32:12 +0530

    add files

---
+++ index.php
@@ -0,0 +1,311 @@
+<!doctype html>
+<html lang="en">
+	<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+
+				<!-- Bootstrap CSS -->
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+					<title>SAI LABELS</title>
+					<!--custom css-->
+				<link rel='stylesheet' href="css\style.css">
+				<!--w3 csss-->
+				<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	</head>
+	<body>
+	<!--header part-->
+	<header>
+		
+		<!--top menu section-->
+		<section>
+			<div>
+				<nav class="navbar navbar-expand-lg navbar-light bg-light" class="container-fluid">
+					
+									<nav class="navbar navbar-light bg-light">
+									  <div class="container">
+										<a class="navbar-brand" href="#">
+										  <img src="images\logo.png" alt="logo of the website" width="200" height="67">
+										</a>
+									  </div>
+									</nav>
+					
+					
+								<div class="container-fluid">
+									 <ul class="navbar-nav me-auto mb-2 mb-lg-0" class="nav navbar-nav navbar-right">
+									 
+										<li class="nav-item">
+										  <a class="nav-link active" aria-current="page" href="#">Home</a>
+										</li>
+									 
+									 
+										<li class="nav-item dropdown">
+										  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+										  PRODUCT LINES
+										  </a>
+										  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+											<li><a class="dropdown-item" href="#">BARCODE</a></li>
+											<li><a class="dropdown-item" href="#">PACKAGING</a></li>
+											<li><hr class="dropdown-divider"></li>
+											<li><a class="dropdown-item" href="#">SERVICES</a></li>
+										  </ul>
+										</li>
+										
+										<li class="nav-item dropdown">
+											<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expand="false">
+												NEWS & INSIGHT
+											</a>
+											
+											<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+												<li><a class="dropdown-item" href="#">NEWS</a></li>
+												<li><a class="dropdown-item" href="#">BLOG</a></li>
+												<li><hr class="dropdown-divider"></li>
+												<li><a class="dropdown-item" href="#">RESOURCE</a></li>
+											
+											</ul>
+										</li>
+										
+										<li class="nav-item dropdown">
+											<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expand="false">
+												ABOUT US
+											</a>
+											
+											<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+												<li><a class="dropdown-item" href="#">ABOUT US</a></li>
+												<li><a class="dropdown-item" href="#">TEAM</a></li>
+												<li><a class="dropdown-item" href="#">PARTENER</a></li>
+												<li><hr class="dropdown-divider"></li>
+												<li><a class="dropdown-item" href="#">CAREER</a></li>
+											</ul>
+										</li>
+										
+										<li class="nav-item">
+										  <a class="nav-link active" aria-current="page" href="#">SUPPORT</a>
+										</li>
+										
+										<button type="button" class="btn btn-outline-success">CONTACT</button>
+									</ul>
+								</div>
+						</nav>
+					</div>
+			 
+		</section>
+	</header>
+	<!--end top enu section-->
+	
+	<main>
+			<!--carosol image-->
+		 
+		<section>		
+			<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+			  <div class="carousel-inner">
+				<div class="carousel-item active">
+				  <img src="https://source.unsplash.com/random" class="d-block w-100" alt="carosol" height="700">
+				</div>
+				<div class="carousel-item">
+				  <img src="https://source.unsplash.com/random" class="d-block w-100" alt="carosol" height="700">
+				</div>
+				<div class="carousel-item">
+				  <img src="https://source.unsplash.com/random" class="d-block w-100" alt="carosol" height="700">
+				</div>
+			  </div>
+			</div>
+		</section>
+	
+			<!--our products area-->
+			
+		<section>
+			
+				<div>
+					<div class="text-center" style="float">
+					  <h1 class="h1">
+						<p>OUR PRODUCTS</p>
+					  </h1>
+					</div>
+					
+					<!--LIST OF PRODUCTS-->
+							<div class="container">
+							  <div class="row">
+							  <!--cards 1-->
+								<div class="col">
+										<div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+											<div class="card" style="width: 18rem;">
+												  <img src="images/barcode.png" class="card-img-top" alt="...">
+												  <div class="card-body">
+													<h5 class="card-title">BARCODE</h5>
+													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+													<a href="#" class="btn btn-primary">EXPLORE</a>
+												  </div>
+											</div>
+										</div>
+								</div>
+								
+								<!--first end-->
+								
+								<!--second card-->
+								<div class="col">
+										<div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+											<div class="card" style="width: 18rem;">
+												  <img src="images/rfid.png" class="card-img-top" alt="..." width="367px" height="167px">
+												  <div class="card-body">
+													<h5 class="card-title">RFID</h5>
+													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+													<a href="#" class="btn btn-primary">EXPLORE</a>
+												  </div>
+											</div>
+										</div>
+								</div>
+								
+								<!--second end-->
+								
+								<!--start third-->
+								<div class="col">
+										<div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+											<div class="card" style="width: 18rem;">
+												  <img src="images/service img.jpg" class="card-img-top" alt="...">
+												  <div class="card-body">
+													<h5 class="card-title">SERVICES</h5>
+													<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+													<a href="#" class="btn btn-primary">EXPLORE</a>
+												  </div>
+											</div>
+										</div>
+								</div>
+								<!--end third-->								
+							  </div>
+							</div>
+					  <!--end list of product-->
+				</div>
+		</section>
+			<!--testimonal section-->
+		<section>
+			<div class="container py-lg-5 py-md-4 py-2">
+				<div class="row">
+				
+					<div class="col-lg-6 mx-auto">
+					<h2 class="h1">OUR CLIENT</h2>
+					<img src="images/client.jpg"></img>
+					</div>
+					
+					<div class="col-lg-6 pr-lg-5 mt-lg-0 mt-sm-5 mt-4">
+						<article class="s-12 m-12 l-6">
+							<h2 class="h2">TESTIMONALS</h2>
+							
+							<p class="lead">
+							  This is a lead paragraph. It stands out from regular paragraphs.
+							  here almost this website is already completed
+							</p>
+						</article>
+					</div>
+				</div>
+			</div>
+		</section>
+					<!--testimonal section end-->
+				<!--OUR ACHIEVMENT-->	
+		<section>
+				<div>
+					<div>
+						<div class="text-center" style="float">
+						  <h1>
+							<p>OUR ACHIEVMENT</p>
+						  </h1>
+						</div>
+					</div>
+				
+					<div  class="container py-lg-5 py-md-4 py-2">
+						<div class="row">
+							
+						
+						
+						<!--iframe-->
+							<div>
+								<iframe width="560" height="315" src="https://www.youtube.com/embed/vCS_Hrm3Yw0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>	
+							</div>
+						</div>
+					
+					</div>
+				
+				</div>
+		</section>
+		<!--END OUR ACHEVMENT-->
+		
+		<!-- footer -->
+			<section class="w3l-footer-29-main">
+			  <div class="footer-29 py-5">
+				<div class="container py-lg-4">
+				  <div class="row footer-top-29">
+					<div class="col-lg-4 col-md-6 col-sm-7 footer-list-29 footer-1 pr-lg-5">
+					  <div class="footer-logo mb-4">
+						<a class="navbar-brand" href="index.html">
+						  <span class="fa fa-clone"></span> SAI LABELS</a>
+					  </div>
+					  <p>We are Creative. World's best  service company. Get the most of reduction in your team’s operating
+						creates amaziG USER FRIENDLY experiences. </p>
+					  <div class="main-social-footer-29 mt-4">
+						<a href="#facebook" class="facebook"><span class="fa fa-facebook"></span></a>
+						<a href="#twitter" class="twitter"><span class="fa fa-twitter"></span></a>
+						<a href="#instagram" class="instagram"><span class="fa fa-instagram"></span></a>
+						<a href="#linkedin" class="linkedin"><span class="fa fa-linkedin"></span></a>
+					  </div>
+					</div>
+					<div class="col-lg-2 col-md-6 col-sm-5 col-6 footer-list-29 footer-2 mt-sm-0 mt-5">
+
+					  <ul>
+						<h6 class="footer-title-29">Navigation</h6>
+						<li><a href="about.html">About Us</a></li>
+						<li><a href="index.html#homeblog"> Blog posts</a></li>
+						<li><a href="services.html">Services</a></li>
+						<li><a href="contact.html">Contact us</a></li>
+					  </ul>
+					</div>
+					<div class="col-lg-2 col-md-6 col-sm-5 col-6 footer-list-29 footer-3 mt-lg-0 mt-5">
+					  <h6 class="footer-title-29">Resources</h6>
+					  <ul>
+						<li><a href="#traning">Marketing Plans</a></li>
+						<li><a href="#marketplace">Digital Services</a></li>
+						<li><a href="#experts">Interior Design</a></li>
+						<li><a href="#platform">Product Selling</a></li>
+						<li><a href="#marketplace">Digital Services</a></li>
+					  </ul>
+
+					</div>
+					<div class="col-lg-4 col-md-6 col-sm-7 footer-list-29 footer-4 mt-lg-0 mt-5">
+					  <h6 class="footer-title-29">Contact Info </h6>
+					  <p>Address : sai labels solution sakol company number 11.</p>
+					  <p class="mt-2">Phone : <a href="#">+91 999898 8734</a></p>
+					  <p class="mt-2">Email : <a href="mailto:info@example.com">email@example.com</a></p>
+					  <p class="mt-2">Support : <a href="#">SAILABELS@support.com</a></p>
+					</div>
+				  </div>
+				</div>
+			  </div>
+			  <!-- copyright -->
+			  <section class="w3l-copyright text-center">
+				<div class="container">
+				  <p class="copy-footer-29">© 2021 SAI LABELS. All rights reserved. Design by <a href="#"
+					  target="_blank">
+					  SAI LABELS</a></p>
+				</div>
+
+				
+				
+			  </section>
+			  <!-- //copyright -->
+			</section>
+			<!-- //footer -->
+	</main>
+		
+	
+	
+	
+	
+	<!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+	
+	</body>
+</html>
